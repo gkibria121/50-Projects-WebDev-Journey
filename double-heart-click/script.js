fd4ccead35650c041d86943b88fcc992247bb569
gkibria121 <gkibria121@gmail.com>
2024-10-28 09:43:31 +0600

    optimize

--- double-heart-click/script.js
+++ double-heart-click/script.js
@@ -7,13 +7,8 @@ cardImage.addEventListener('dblclick',(event)=>{
  
     const x = event.offsetX;
     const y = event.offsetY; 
-    console.log(event);
-    
-    const heartEl = createHeart();
-    heartEl.style.top = `${y}px`;
-    heartEl.style.left = `${x}px`;
-    cardImage.appendChild(heartEl);
-    
+    const heartEl = createHeart(x,y);
+    cardImage.appendChild(heartEl); 
     heartCountEl.innerText = ++times;
     setTimeout(()=>{
         heartEl.remove(); 
@@ -23,10 +18,12 @@ cardImage.addEventListener('dblclick',(event)=>{
  
 
 
-function createHeart(){
+function createHeart(x,y){
     const heartEl = document.createElement('span')
     heartEl.classList.add('icon');
     heartEl.classList.add('icon--heart');
     heartEl.classList.add('animate'); 
+    heartEl.style.left = `${x}px`;
+    heartEl.style.top = `${y}px`;
     return heartEl;
 }
