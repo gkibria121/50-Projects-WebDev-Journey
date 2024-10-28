302547ca61c338d8e1d4f63563fb08e51b724995
gkibria121 <gkibria121@gmail.com>
2024-10-28 10:55:17 +0600

    update logic

--- auto-text-effect/script.js
+++ auto-text-effect/script.js
@@ -3,35 +3,30 @@ const speedControlEl = document.querySelector('input[type="number"]')
 const heroText = "We Love Programming!";
 
 let speed = 100; 
+let idx = 1;
 
 
-
-
-animateText(heroTextEl,heroText)
-
-
-speedControlEl.addEventListener('change',handleSpeedChange)
  
+speedControlEl.addEventListener('input',handleSpeedChange)
+ 
+writeText()
 
-function animateText(elment, text){
-    heroTextEl.innerText = "";
-    let textArr = text.split("");
-
-    for (let i in textArr){ 
-        setTimeout(()=>{
-            const span = document.createElement('span')
-            span.innerText = textArr[i]; 
-            elment.appendChild(span); 
-        },speed*i) 
-
+function writeText(){
+    heroTextEl.innerText = heroText.slice(0,idx) 
+    if(idx<heroText.length ){
+        idx++
+        
     }
-    
+    else{
+        idx =1;
+    }
+    setTimeout(writeText,speed)
 }
 
+ 
 function handleSpeedChange(event){
 
     
     speed = 100/event.target.value;
-    animateText(heroTextEl,heroText)
-
+    
 }
