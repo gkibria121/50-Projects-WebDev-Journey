9134caf63a75b34db24df8a833b5eec17e14b641
gkibria121 <gkibria121@gmail.com>
2024-10-28 09:39:06 +0600

    add double click heart

---
+++ double-heart-click/script.js
@@ -0,0 +1,38 @@
+const cardImage = document.querySelector('.card .card-image')
+const heartCountEl = document.getElementById("heart-count")
+let times = 0;
+
+
+cardImage.addEventListener('dblclick',(event)=>{
+ 
+    const x = event.offsetX;
+    const y = event.offsetY; 
+    console.log(event);
+    
+    const heartEl = createHeart();
+    heartEl.style.top = `${y}px`;
+    heartEl.style.left = `${x}px`;
+    cardImage.appendChild(heartEl);
+    
+    updateCount()
+    setTimeout(()=>{
+        heartEl.remove(); 
+    },1500)
+    
+})
+
+
+
+function updateCount(){
+    times++;
+    heartCountEl.innerText = times;
+}
+
+
+function createHeart(){
+    const heartEl = document.createElement('span')
+    heartEl.classList.add('icon');
+    heartEl.classList.add('icon--heart');
+    heartEl.classList.add('animate'); 
+    return heartEl;
+}
