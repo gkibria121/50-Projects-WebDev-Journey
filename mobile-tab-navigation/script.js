ccbceddeb23c68140c23ef32261c82f735899647
gkibria <gkibria121@gmail.com>
2024-11-18 10:41:28 +0600

    optimize

--- mobile-tab-navigation/script.js
+++ mobile-tab-navigation/script.js
@@ -1,16 +1,17 @@
  
-const images = document.querySelectorAll('.image');
+const images = document.querySelectorAll('.content');
 
 
-const buttons = document.querySelectorAll('button')
+const buttons = document.querySelectorAll('.nav-link')
 
 
 buttons.forEach((button,idx)=>{
 
     button.addEventListener('click',()=>{
         document.querySelectorAll('.active').forEach(el=>el.classList.remove('active'))
+        document.querySelectorAll('.show').forEach(el=>el.classList.remove('show'))
         button.classList.add('active')
-        images[idx].classList.add('active') 
+        images[idx].classList.add('show') 
         
     })
 })
